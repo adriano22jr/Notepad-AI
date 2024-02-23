57be89f001e25838d26c04738ad3099fbd32da7b
adriano22_ <califano993@gmail.com>
2024-02-23 20:01:11 +0100

    test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import flask, os
+import flask, os, requests
 from google_auth_oauthlib.flow import Flow
 from google.oauth2 import id_token
 from pip._vendor import cachecontrol
@@ -6,12 +6,13 @@ import google.auth.transport.requests
 
 app = flask.Flask(__name__, template_folder = "templateFiles", static_folder = "staticFiles")
 app.secret_key = 'notepadai'
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 client_id = os.environ["GOOGLE_CLIENT_ID"]
 client_config = os.environ["GOOGLE_PROVIDER_AUTHENTICATION_SECRET"]
 
 flow = Flow.from_client_config(client_config = client_config, 
                                scopes = ["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-                               redirect_uri = "http://127.0.0.1:8080/callback")
+                               redirect_uri = "https://notepad-ai.azurewebsites.net/callback")
 
 def login_required(function):
     def wrapper(*args, **kwargs):
@@ -45,7 +46,7 @@ def callback():
         flask.abort(500)
     
     credentials = flow.credentials
-    request_session = flask.request.session()
+    request_session = requests.session()
     cached_session = cachecontrol.CacheControl(request_session)
     token_request = google.auth.transport.requests.Request(session = cached_session)
     
@@ -67,7 +68,6 @@ def notebook_regular():
     return flask.render_template("notebook_regular.html")
 
 @app.route("/notebook-markdown", methods = ["GET", "POST"])
-@login_required
 def notebook_markdown():
     return flask.render_template("notebook_markdown.html")
 
