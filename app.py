b800cccf02d1d8d799e4f5824c84876ebfee3e2a
adriano22_ <califano993@gmail.com>
2024-04-16 16:58:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import scripts.container_operations as container_ops
 import scripts.ai_services as ai_services
 
 app = flask.Flask(__name__, template_folder = "templateFiles", static_folder = "staticFiles")
-os.system("sudo apt-get install libasound2-dev")
+os.system("apt-get install -y libasound2-dev")
 
 app.secret_key = app_config.APP_SECRET_KEY
 client_id = app_config.GOOGLE_CLIENT_ID
